b019c7bd450522f5bf6fead249830395b1306597
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-03-21 19:07:17 -0700

    Aded 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the standard output
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
