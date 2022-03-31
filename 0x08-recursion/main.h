bc1f62c572fa23bb81ee43faeafeb6f537278673
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-03-30 20:11:55 -0700

    Added 4-pow_recursion.c, 5-sqrt_recursion.c, 6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,3 +2,6 @@ int _putchar(char);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
